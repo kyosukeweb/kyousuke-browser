62144e433aded018428d6347f2a11fb192e4d706
themirrazz <89490659+themirrazz@users.noreply.github.com>
2025-03-24 11:27:13 -0700

    branding

---
+++ src/browser/branding/unofficial/pref/firefox-branding.js
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This file contains branding-specific prefs.
+
+pref("startup.homepage_override_url", "");
+pref("startup.homepage_welcome_url", "about:welcome");
+pref("startup.homepage_welcome_url.additional", "");
+// The time interval between checks for a new version (in seconds)
+pref("app.update.interval", 86400); // 24 hours
+// Give the user x seconds to react before showing the big UI. default=24 hours
+pref("app.update.promptWaitTime", 86400);
+// URL user can browse to manually if for some reason all update installation
+// attempts fail.
+pref("app.update.url.manual", "https://kyousuke.vercel.app/download/");
+// A default value for the "More information about this update" link
+// supplied in the "An update is available" page of the update wizard.
+pref("app.update.url.details", "https://kyousuke.vercel.app/");
+
+// The number of days a binary is permitted to be old
+// without checking for an update.  This assumes that
+// app.update.checkInstallTime is true.
+pref("app.update.checkInstallTime.days", 2);
+
+// Give the user x seconds to reboot before showing a badge on the hamburger
+// button. default=immediately
+pref("app.update.badgeWaitTime", 0);
+// This represents the duration between an update being ready and it being
+// possible to install it while other sessions are running. Note that
+// having this pref's duration differ from `app.update.badgeWaitTime` may result
+// in undefined behavior such as showing an update prompt that does not result
+// in an update when the "Restart to Update" button is clicked. Keep in mind
+// that this is in milliseconds and `app.update.badgeWaitTime` is in seconds.
+// Note that the effective value of this pref is limited to 1 week, maximum.
+pref("app.update.multiSessionInstallLockout.timeoutMs", 0);
+
+// Number of usages of the web console.
+// If this is less than 5, then pasting code into the web console is disabled
+pref("devtools.selfxss.count", 5);
+
+pref("app.releaseNotesURL", "https://kyousuke.vercel.app/fxr/upgrade.html?v=%VERSION%");
